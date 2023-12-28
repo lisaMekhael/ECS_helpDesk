f427e6ddb2fe73cedaa52e2187df04742af53608
Yassa122 <yassa.ashraf@yahoo.com>
2023-12-28 02:15:59 +0200

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, 'build'), // Specify the output directory as 'build'
+    outDir: resolve(__dirname, "build"), // Specify the output directory as 'build'
   },
 });
